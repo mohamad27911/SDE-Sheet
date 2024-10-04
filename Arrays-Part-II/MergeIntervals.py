4ce1b6c64bd7c3ac44c30bb75ff850319885f393
mohamad27911 <mohamad.mar72@gmail.com>
2024-10-04 16:16:41 +0300

    Merge Intervals

---
+++ Arrays-Part-II/MergeIntervals.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def merge(self, intervals):
+        intervals.sort(key=lambda x: x[0])
+        merged=[]
+        for interval in intervals:
+            if not merged or merged[-1][1]<interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1]=[merged[-1][0],max(merged[-1][-1],interval[1])]
